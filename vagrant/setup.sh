8015cb3791e91dce0ceb71faed04fb1759c9ecfb
Scott Mebberson <scott@scottmebberson.com>
2016-02-09 12:03:57 +1030

    Refined scripts for testing, test coverage and travis build testing.

--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -5,8 +5,15 @@ then
 
 	echo ">>> setting up virtual machine"
 
-	# install mocha
-	npm install -g mocha
+	# alias the test-coverage script
+	if [ ! -e /usr/local/bin/test-coverage ]; then
+		ln -s /vagrant/vagrant/scripts/test-coverage /usr/local/bin/test-coverage
+	fi
+
+	# alias the test-travis script
+	if [ ! -e /usr/local/bin/test-travis ]; then
+		ln -s /vagrant/vagrant/scripts/test-travis /usr/local/bin/test-travis
+	fi
 
 	echo ">>> virtual machine has been setup and is ready to go'"
 
